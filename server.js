7eca5e0ac679aa344520b7cec0b30bccde7d0453
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-08-02 18:40:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-
+const port = process.env.PORT || 3000
 const ourWord = "lucky";
 
 app.get("/word/:guess", function (req, res) {
@@ -39,4 +39,4 @@ app.get("/word/:guess", function (req, res) {
 });
 app.use(express.static("public"));
 
-app.listen(3000);
+app.listen(port);
